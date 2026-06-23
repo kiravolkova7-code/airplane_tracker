f5297c5642836e12299c2d58e06182aab74aaf5d
august <kira_volkova_7@mail.ru>
2026-06-23 23:15:55 +0300

    tests for user_interaction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # main.py
 
 from src.user_interaction import main
+from src.working_with_API import OpenSkyApiClient, NominatimApiClient
+from src.airplane_info import AirplaneInfo, JsonAirplaneStorage
+import time
 
 if __name__ == "__main__":
     main()
